9fb4170b179b05c6a67ed2f5e2234dc69c15428f
mzdu <daniel_pub@hotmail.com>
2015-12-06 23:49:43 -0500

    Adding new comment service is working now

--- controllers/comment.js
+++ controllers/comment.js
@@ -10,10 +10,11 @@ exports.addComment = function(req, res) {
 //	POST
 //	http://localhost:8080/api/status/563684f2853e6411003a1a2f/comment
 	
+	// statusID is retrieved from url
 	var statusID = req.params.statusID;
 	var commentUserID = req.body.userID;
 	var commentMsg = req.body.comment;
-	
+
 	// find the status to add new comment
 	var filter = {
 		_id: statusID
@@ -23,27 +24,59 @@ exports.addComment = function(req, res) {
     	commentBy: commentUserID,
 	    comment: commentMsg 
 	  }
-	// use mongoose method
-    Comment.create(insertObj, function(err, results) {
+	// use mongoose method to generate a new comment
+	// comment: _id, CommentBy, Comment, CreatedDate
+    var promise = Comment.create(insertObj, function(err, results) {
         if (err) {
             console.log('in err', err);
             return utility.handleError(res, err);
-        } else {
+        } 
+        
+        else {
         	
+        	// here we get the newly generated commentID
         	var commentID = results._id;
+//            return res.send({
+//                message: commentID
+//            });	
+            
         	var updateObj = {
         		$push: { comments:commentID}
         	};
-
+        	
+//        	console.log('show vars before status update');
+//        	console.log(statusID);
+//        	console.log(updateObj);
+        	
+        	// the next step is to add this commentID into status comments list
+        	// 1. find the statusID that status needs to be updated
+        	// 2. add the commentID in updateObj then update the status
+        	Status.findByIdAndUpdate(statusID, updateObj,function(err, results) {
+	                if (err) {
+	                    console.log('in err', err);
+	                    return utility.handleError(results, err);
+	                } else {
+	                    console.log('in status upate else');
+	                    return res.send(results);
+	                }
+	        });       
+        	
         }
-    });
-   
+      });
+	
+//    return res.send({
+//        message: commentID
+//    });	
+   // add this comment to the status
+   // status: statusID, ownerID, location, createdDate, likedBy[], comments[]
     
 //    Status.update();
     
 }
 
 exports.getComments = function(req, res) {
+//	GET
+//	http://localhost:8080/api/status/563684f2853e6411003a1a2f/comments	
     return res.send({
         message: 'getComments Successful'
     });
